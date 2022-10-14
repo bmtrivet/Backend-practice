3d57e6f4fc445e788da6f4c95a32f04840dd2bb9
Nikita_Sirodoev <syrodoev9@gmail.com>
2022-10-14 20:02:03 +0300

    qww

--- db.js
+++ db.js
@@ -1,17 +1,16 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize(
-  process.env.DB_DATABASE_PROD,
-  process.env.DB_USER_PROD,
-  process.env.DB_PASSWORD_PROD,
-  {
-    dialect: "postgres",
-    host: process.env.DB_HOST_PROD,
-    port: process.env.DB_PORT,
-    logging: false,
+module.exports = new Sequelize({
+  database: process.env.DB_DATABASE_PROD,
+  username: process.env.DB_USER_PROD,
+  password: process.env.DB_PASSWORD_PROD,
+  host: process.env.DB_HOST_PROD,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+  dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
     },
-  }
-);
+  },
+});
